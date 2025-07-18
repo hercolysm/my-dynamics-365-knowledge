050fde51f2d1edbecc8fc9066fc751a6a6c21439
Hercolys Moraes Araujo <avanade.hercolys@grupofleury.com.br>
2025-07-18 19:15:06 -0300

    Add project files.

---
+++ Plugins/Opportunity.cs
@@ -0,0 +1,7 @@
+﻿namespace Plugins
+{
+    public class Opportunity
+    {
+
+    }
+}
---
+++ Tests/Opportunity.cs
@@ -0,0 +1,43 @@
+﻿namespace Tests
+{
+    [TestClass]
+    public sealed class Opportunity
+    {
+        [TestMethod]
+        public void WinOpportunityRequestTest()
+        {
+            Guid opportunityId = new Guid("{C2986E6E-3BE4-4DD1-9D40-A97AA4206199}");
+
+            var request = new WinOpportunityRequest
+            {
+                OpportunityClose = new Entity("opportunityclose")
+                {
+                    Attributes = {
+                        {
+                            "opportunityid",
+                            new EntityReference("opportunity", opportunityId)
+                        }
+                        // Add additional opportunityclose column 
+                        // data to provide more details.
+                    }
+                },
+                // Won is the default and only status.
+                // Custom opportunity status options
+                // can be created that also represent different types
+                // of win.
+                Status = new OptionSetValue(3) //Won
+            };
+
+            service.Execute(request);
+        }
+
+        [TestMethod]
+        public void UpdateOpportunityStatecodeTest()
+        {
+            Entity opportunity = new Entity("opportunity", new Guid("{C2986E6E-3BE4-4DD1-9D40-A97AA4206199}"));
+            opportunity["statecode"] = new OptionSetValue(1); // Status - Ganho
+            opportunity["statuscode"] = new OptionSetValue(860090005); // Razão do Status - Ganhos do Mês
+            service.Update(opportunity);
+        }
+    }
+}
